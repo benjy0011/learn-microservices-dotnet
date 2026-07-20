15c49298ee0e9f9602527caebd47f44875e29abf
benjy0011 <152722201+benjy0011@users.noreply.github.com>
2026-07-20 23:47:43 +0800

    feat(search): add search controller

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Controllers;
+
+[ApiController]
+[Route("api/search")]
+public class SearchController : ControllerBase
+{
+    // MongoDb lib is a static class, no dependancy injection needed
+    
+
+    [HttpGet]
+    public async Task<ActionResult<List<Item>>> SearchItems([FromQuery]string searchTerm)
+    {
+        var query = DB.Find<Item>();
+
+        query.Sort(x => x.Ascending(a => a.Make));
+
+        if (!string.IsNullOrEmpty(searchTerm))
+        {
+            query.Match(Search.Full, searchTerm).SortByTextScore();
+        }
+
+        var result = await query.ExecuteAsync();
+
+        return result;
+    }
+}
