101dbc5cb3eec5b33ea75e09a262c0d9a3719372
benjy0011 <152722201+benjy0011@users.noreply.github.com>
2026-08-10 23:46:55 +0800

    feat(navbar): Creating a nav bar

--- frontend/web-app/app/layout.tsx
+++ frontend/web-app/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import NavBar from "./nav/NavBar";
 
 export const metadata: Metadata = {
   title: "Carsties",
@@ -11,7 +12,15 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     <html
       lang="en"
     >
-      <body>{children}</body>
+      <body>
+        <NavBar />
+
+        <main className="container mx-auto px-5 pt-10">
+          {children}
+        </main>
+
+
+      </body>
     </html>
   );
 }
--- frontend/web-app/app/page.tsx
+++ frontend/web-app/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-semibold">Hello World</h1>
+      Content
     </div>
   );
 }
