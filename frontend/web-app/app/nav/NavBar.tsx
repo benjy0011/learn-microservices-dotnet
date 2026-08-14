3e78a1d777e4dc3490fb9b2db2fc3a47d521f56e
benjy0011 <152722201+benjy0011@users.noreply.github.com>
2026-08-14 23:36:47 +0800

    feat(fe): Adding a search bar

--- frontend/web-app/app/nav/NavBar.tsx
+++ frontend/web-app/app/nav/NavBar.tsx
@@ -1,4 +1,5 @@
 import { AiOutlineCar } from "react-icons/ai"
+import Search from "./Search"
 
 export default function NavBar() {
   return (
@@ -7,7 +8,7 @@ export default function NavBar() {
         <AiOutlineCar size={34} />
         <div>Carsties Auctions</div>
       </div>
-      <div>Search</div>
+      <Search />
       <div>Login</div>
     </header>
   )
