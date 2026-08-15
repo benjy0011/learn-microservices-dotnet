3638bf3d8b4e05b3f8d48ecc850d9957d4792e31
benjy0011 <152722201+benjy0011@users.noreply.github.com>
2026-08-15 23:00:44 +0800

    feat(feAuth): Installing Next Auth into client app

---
+++ frontend/web-app/auth.ts
@@ -0,0 +1,20 @@
+import NextAuth, { Profile } from "next-auth"
+import { OIDCConfig } from "next-auth/providers"
+import DuendeIDS6Provider from "next-auth/providers/duende-identity-server6"
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    DuendeIDS6Provider({
+      id: 'id-server',
+      clientId: "nextApp", // match the id configured in Identity Svc
+      clientSecret: "secret", // this file is server side code, its save
+      issuer: "http://localhost:5001",
+      authorization: {
+        params: {
+          scope: 'openid profile auctionApp'
+        }
+      },
+      idToken: true
+    } as OIDCConfig<Profile>),
+  ]
+})
