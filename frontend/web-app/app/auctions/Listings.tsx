ad0d3a04aaa9bbb455b1bd5de981d55f914499cf
benjy0011 <152722201+benjy0011@users.noreply.github.com>
2026-08-15 23:43:57 +0800

    style(feListing): improve styling

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -46,7 +46,7 @@ export default function Listings() {
         <EmptyFilter showReset />
       ) : (
         <>
-          <div className="grid grid-cols-4 gap-6">
+          <div className="grid grid-cols-4 gap-6 max-lg:grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1">
             {data && data.results.map((auction) => (
               <AuctionCard
                 key={`${auction.id}-${params.orderBy}`}
