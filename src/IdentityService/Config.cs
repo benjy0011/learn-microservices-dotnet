adf2feeccd385bb43041ed792102ce05d22fe849
benjy0011 <152722201+benjy0011@users.noreply.github.com>
2026-08-15 21:38:32 +0800

    refactor(identity): Includes the user's claims in the ID token so the client can use them without an extra user-info request.

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -40,6 +40,7 @@ public static class Config
                 AllowOfflineAccess = true,
                 AllowedScopes = {"openid", "profile", "auctionApp"},
                 AccessTokenLifetime = 3600*24*30,
+                AlwaysIncludeUserClaimsInIdToken = true, // Includes the user's claims in the ID token so the client can use them without an extra user-info request.
             }
         };
 }
