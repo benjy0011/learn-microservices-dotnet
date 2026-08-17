349ba27f2b97767f13941e777158f33bc2714a8a
benjy0011 <152722201+benjy0011@users.noreply.github.com>
2026-08-17 21:49:31 +0800

    feat(fe): Routing in NextJS

--- frontend/web-app/app/nav/UserActions.tsx
+++ frontend/web-app/app/nav/UserActions.tsx
@@ -23,7 +23,9 @@ export default function UserActions({
         Auctions won
       </DropdownItem>
       <DropdownItem icon={AiFillCar}>
-        Sell my car
+        <Link href="/auctions/create">
+          Sell my car
+        </Link>
       </DropdownItem>
       <DropdownItem icon={HiCog}>
         <Link href='/session'>
